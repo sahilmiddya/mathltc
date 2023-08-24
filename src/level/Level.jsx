12deee069df50cd1ab6423ae3acdd0381c6324c5
Gaurav <bhardwajgaurav204@gmail.com>
2023-08-24 13:13:46 +0530

    .

--- src/level/Level.jsx
+++ src/level/Level.jsx
@@ -31,18 +31,20 @@ const Level = () => {
   return (
     <div>
       <Navbar />
-      {quizLevel?.map?.((e) => (
-        <>
-          <div
-            className="level"
-            onClick={() => {
-              goto(e.title, e, e.quiz_format.slug);
-            }}
-          >
-            <div className="qtitle">{e?.title}</div>
-          </div>
-        </>
-      ))}
+      <div className="levelcont">
+        {quizLevel?.map?.((e) => (
+          <>
+            <div
+              className="level"
+              onClick={() => {
+                goto(e.title, e, e.quiz_format.slug);
+              }}
+            >
+              <div className="ltitle">{e?.title}</div>
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
