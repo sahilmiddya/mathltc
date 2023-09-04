5f2f98e78fd519fbfbf9d8f5ceabad03ff927985
madhushraba <madhushraba21das@gmail.com>
2023-09-04 19:34:48 +0530

    settingui

--- src/settings/Settingsui.jsx
+++ src/settings/Settingsui.jsx
@@ -5,16 +5,18 @@ import { deleteAccAsync } from "../store/ProfileActions";
 import { toast } from "react-toastify";
 import { logoutUser } from "../store/authSlice";
 import { logoutUserAction } from "../store/authActions";
+import { useState } from "react";
 
 const Settingsui = () => {
+  const [isOn, setIsOn] = useState(false);
   const auth = useSelector((state) => state.auth);
   let dispatch = useDispatch();
 
   const closemodal = () => {
     dispatch(setsettingModalOpen(false));
   };
- 
-  const deleteac =()=>{
+
+  const deleteac = () => {
     dispatch(
       deleteAccAsync(
         auth.user.token,
@@ -23,28 +25,61 @@ const Settingsui = () => {
         },
         (msg) => {
           // nav("/map");
-          dispatch(logoutUser())
-          
-          toast.success(msg.detail)
+          dispatch(logoutUser());
+
+          toast.success(msg.detail);
         },
         (err) => {
-          toast.error(err.detail)
+          toast.error(err.detail);
         }
       )
     );
-  }
+  };
   return (
     <>
       <div className="settingbody">
-        <div className="left">left</div>
+        <div className="left">
+          <div className="range_one">
+            <p>some heading</p>
+            <div className="timers">
+              <div className="set_btn1">btn1</div>
+              <div className="set_btn1">btn1</div>
+              <div className="set_btn1">btn1</div>
+            </div>
+            <div className="range">
+              <input type="range" />
+            </div>
+            <div className="onoffbtns">
+              {/* ===============================================
+               */}
+
+              {/* <input
+                type="checkbox"
+                checked={isOn}
+                onClick={(e) => setIsOn(e.target.val)}
+              />
+              <br />
+              <button onClick={() => setIsOn((prevState) => !prevState)}>
+                Toggle
+              </button> */}
+              {/* =============================================== */}
+            </div>
+          </div>
+        </div>
         <div className="right">
-          <p>
-          avatar and usernames</p>
-          <button className="setbtn3"  onClick={() => {
-                dispatch(logoutUserAction());
-              }}>Logout</button>
+          <p>avatar and usernames</p>
+          <button
+            className="setbtn3"
+            onClick={() => {
+              dispatch(logoutUserAction());
+            }}
+          >
+            Logout
+          </button>
           <button className="setbtn3">Change Password</button>
-          <button className="setbtn3" onClick={deleteac}>Delete Account</button>
+          <button className="setbtn3" onClick={deleteac}>
+            Delete Account
+          </button>
         </div>
       </div>
 
