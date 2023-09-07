0360f4d532e34539065ad9f54a595973a58ef3c8
madhushraba <madhushraba21das@gmail.com>
2023-09-07 18:23:56 +0530

    nvfnv

--- src/leaderboard/Leaderboard.jsx
+++ src/leaderboard/Leaderboard.jsx
@@ -3,6 +3,7 @@ import Navbar from "../home/navbar/Navbar";
 import "./leader.css";
 import { leader_Async } from "../store/leaderaction";
 import { useDispatch, useSelector } from "react-redux";
+import { baseURL } from "../constants/baseURL";
 
 const Leaderboard = () => {
   const dispatch = useDispatch();
@@ -10,6 +11,9 @@ const Leaderboard = () => {
     (state) => state.leaderboard?.leaderboard_data?.leaderboard
   );
   // console.log(leaderboard_data.id);
+  // const user_avatar = useSelector(
+  //   (state) => state.userdetails.user.avatar.image
+  // );
   const auth = useSelector((state) => state.auth);
   useEffect(() => {
     dispatch(
@@ -33,16 +37,35 @@ const Leaderboard = () => {
             {Array.isArray(leaderboard_data) &&
               leaderboard_data.map((item, index) => (
                 <div
+                  className="lb_row"
                   key={item.id}
                   style={{ display: "flex", justifyContent: "space-between" }}
                 >
                   <div className="col_left">
                     <div className="cols col1">{index + 1}</div>
+                    <div
+                      className="cols col1"
+                      style={{
+                        backgroundImage: `url(${baseURL + item?.user?.background?.image})`,
+                        // width: "70px",
+                        // padding:'12px',
+                        // height: "40px",
+                        borderRadius: "50%",
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img
+                        src={baseURL + item?.user?.avatar?.image}
+                        alt="gtfh"
+                      />
+                    </div>
                     <div className="cols col1" style={{ minWidth: 150 }}>
-                      {" "}
                       {item.user.username}
                     </div>
-                    <div className="cols col1"> fvgfv</div>
                   </div>
                   <div className="col_right">
                     <div className="cols col1"> {item.animal_status}</div>
